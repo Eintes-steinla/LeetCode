60429d00d5f1f0e78b999c402272fc62cc99f3a4
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-05-06 13:36:54 +0700

    20_ValidParentheses

---
+++ Stack/20_ValidParentheses/solution1.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        // Khi gặp ngoặc mở (, [, { → push vào stack.
+        // Khi gặp ngoặc đóng ), ], } → kiểm tra top() có khớp không, rồi pop.
+
+        int n = s.length();
+        stack<char> brackets;
+        for (char i : s)
+        {
+            if (i == '{' || i == '[' || i == '(')
+            {
+                brackets.push(i);
+            }
+            if (brackets.empty())
+                return false;
+            if ((i == '}' && brackets.top() == '{') ||
+                (i == ']' && brackets.top() == '[') ||
+                (i == ')' && brackets.top() == '('))
+            {
+                brackets.pop();
+                n -= 2;
+            }
+        }
+        if (n != 0)
+            return false;
+
+        return brackets.empty() ? true : false;
+    }
+};
